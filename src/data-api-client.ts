0872e4bd241a38ee728346cdd37a4373b9dd133a
Atuki <hotlvr9@gmail.com>
2023-06-01 19:23:10 +0900

    refactor: move fetcher into client class

--- src/data-api-client.ts
+++ src/data-api-client.ts
@@ -10,56 +10,13 @@ import {
 import { GeneralResponse } from './types/common';
 import urlJoin from 'url-join';
 
-const fetcher = async <T>({
-  method,
-  url,
-  apiKey,
-  params,
-  body,
-}: {
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
-  url: string;
-  apiKey: string;
-  params?: Record<string, string>;
-  body?: unknown;
-}) => {
-  const fullUrl = urlJoin(url, new URLSearchParams(params).toString());
-
-  const error = new Error();
-
-  try {
-    const response = await fetch(fullUrl, {
-      method,
-      headers: {
-        'X-API-KEY': apiKey,
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      ...(method !== 'GET' ? { body: JSON.stringify(body) } : {}),
-    });
-
-    if (!response.ok) {
-      const body = await response.json();
-      if (body && typeof body === 'object' && 'message' in body) {
-        error.message = body.message;
-      } else {
-        error.message = `HTTP error, status = ${response.status}`;
-      }
-      throw error;
-    }
-
-    return (await response.json()) as T;
-  } catch (e) {
-    throw e;
-  }
-};
-
 class MorphDataAPIClient {
   public async queryRecords<R extends MorphRecord = EmptyRecord>(
     url: string,
     apiKey: string,
     options: QueryRecordOptions<R>
   ): Promise<QueryRecordListResponse<R>> {
-    return await fetcher({ url, method: 'POST', apiKey, body: options });
+    return await this.fetcher({ url, method: 'POST', apiKey, body: options });
   }
 
   public async createRecord<R extends MorphRecord = MorphRecord>(
@@ -67,7 +24,7 @@ class MorphDataAPIClient {
     apiKey: string,
     options: CreateRecordOptions<R>
   ): Promise<GeneralResponse> {
-    return await fetcher({ url, method: 'POST', apiKey, body: options });
+    return await this.fetcher({ url, method: 'POST', apiKey, body: options });
   }
 
   public async updateRecords<R extends MorphRecord = MorphRecord>(
@@ -75,7 +32,7 @@ class MorphDataAPIClient {
     apiKey: string,
     options: UpdateRecordOptions<R>
   ): Promise<GeneralResponse> {
-    return await fetcher({ url, method: 'POST', apiKey, body: options });
+    return await this.fetcher({ url, method: 'POST', apiKey, body: options });
   }
 
   public async deleteRecords<R extends MorphRecord = MorphRecord>(
@@ -83,7 +40,7 @@ class MorphDataAPIClient {
     apiKey: string,
     options: DeleteRecordOptions<R>
   ): Promise<GeneralResponse> {
-    return await fetcher({ url, method: 'POST', apiKey, body: options });
+    return await this.fetcher({ url, method: 'POST', apiKey, body: options });
   }
 
   public async chatReply(
@@ -97,12 +54,58 @@ class MorphDataAPIClient {
       prompt: string;
     }
   ) {
-    return await fetcher({
+    return await this.fetcher({
       url,
       method: 'POST',
       apiKey,
       body: { prompt, botId },
     });
+  }
+
+  /**
+   * fetcher
+   */
+  private async fetcher<T>({
+    method,
+    url,
+    apiKey,
+    params,
+    body,
+  }: {
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    url: string;
+    apiKey: string;
+    params?: Record<string, string>;
+    body?: unknown;
+  }) {
+    const fullUrl = urlJoin(url, new URLSearchParams(params).toString());
+
+    const error = new Error();
+
+    try {
+      const response = await fetch(fullUrl, {
+        method,
+        headers: {
+          'X-API-KEY': apiKey,
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        ...(method !== 'GET' ? { body: JSON.stringify(body) } : {}),
+      });
+
+      if (!response.ok) {
+        const body = await response.json();
+        if (body && typeof body === 'object' && 'message' in body) {
+          error.message = body.message;
+        } else {
+          error.message = `HTTP error, status = ${response.status}`;
+        }
+        throw error;
+      }
+
+      return (await response.json()) as T;
+    } catch (e) {
+      throw e;
+    }
   }
 }
 

e6d7bd08d735ba576d1a5eb95f10e1bfadcca5bf
Atuki <hotlvr9@gmail.com>
2024-01-23 14:11:31 +0900

    chore: tsdx → tsc & rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+  {
+    input: './src/index.ts',
+    output: {
+      file: './dist/index.js',
+      format: 'esm',
+    },
+    plugins: [
+      typescript({
+        outDir: './dist/',
+      }),
+    ],
+  },
+];
